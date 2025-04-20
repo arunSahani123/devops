50539641b12d2e668f57f62a4e58bd2b890e63e9
delta student <arunsahani258@gmail.com>
2025-04-20 23:56:58 +0530

    Fix: ESLint issues in AppReducer.jsx for CI/CD

--- src/context/AppReducer.jsx
+++ src/context/AppReducer.jsx
@@ -1,16 +1,21 @@
-export default (state , action) =>{
-    switch(action.type){
-       case 'Add_Transection':
+const appReducer = (state, action) => {
+    switch (action.type) {
+      case 'Add_Transection':
         return {
-            ...state,
-            transection : [action.payLoad , ...state.transection]
-        } 
-        case  'Delete_Transection'  :
-            return{
-                ...state,
-              transection : state.transection.filter(obj => obj.id != action.payLoad)
-            } 
-       default :
-       return state;
+          ...state,
+          transection: [action.payLoad, ...state.transection],
+        };
+  
+      case 'Delete_Transection':
+        return {
+          ...state,
+          transection: state.transection.filter(obj => obj.id !== action.payLoad),
+        };
+  
+      default:
+        return state;
     }
-}
+  };
+  
+  export default appReducer;
+  
